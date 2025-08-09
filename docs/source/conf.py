6fd9c926af5a5edd9b9ddc26d90a7e9cdbefaca5
william <william@freepackets.net>
2025-08-09 17:49:10 +0800

    Initial commit

---
+++ docs/source/conf.py
@@ -0,0 +1,41 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Lumache'
+copyright = '2025, Graziella'
+author = 'Graziella'
+release = '0.1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'alabaster'
+html_static_path = ['_static']
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    'sphinx.ext.duration',
+]
+
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'furo'
---
+++ docs/source/usage.rst
@@ -0,0 +1,24 @@
+Usage
+=====
+
+Installation
+------------
+
+To use Lumache, first install 
+
+
+it using pip:
+
+.. code-block:: console
+      
+   (.venv) $ pip install lumache
+
+Usage
+=====
+
+.. _installation:
+
+Installation
+------------
+
+...
