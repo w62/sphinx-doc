582784e4da1eee242b236517ec41fce12d5bbc34
william <william@freepackets.net>
2025-08-09 22:15:52 +0800

    Add some files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,12 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here.
+import sys
+from pathlib import Path
+sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
+
 project = 'Lumache'
 copyright = '2025, Graziella'
 author = 'Graziella'
@@ -32,12 +38,14 @@ html_static_path = ['_static']
 # ones.
 extensions = [
     'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
 ]
 
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'furo'
+#html_theme = 'furo'
 
 #
---
+++ lumache.py
@@ -0,0 +1,12 @@
+def get_random_ingredients(kind=None):
+    """"
+    Return a list of random ingredients as strings.
+    
+    :param kind: Optional "kind" of ingredients.
+    :type kind: list[str] or None
+    :raise lumache.InvalidKindError: If the kind is invalid.
+    :return: The ingredients list.
+    :rtype: list[str]
+    """
+
+    return ["eggs", "bacon", "spam"]
